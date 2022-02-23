4b18f80c732a8953d33c90266d7e725858170ad9
Kojo Boakye-Yiadom <kwadwoneer@gmail.com>
2022-02-23 13:59:32 -0700

    Server deployment for landingPage drill

--- server/index.js
+++ server/index.js
@@ -4,8 +4,13 @@ const path = require('path');
 
 const app = express();
 
-
+const port = process.env.PORT || 3003;
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../public','landing.html'));
+})
+
+app.use(express.static(path.join(__dirname,'../public')))
+app.listen(port, () => {
+    console.log(`Port at ${port}`);
 })
